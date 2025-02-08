05a4eb920879aa1630bc24eae19dc8843b4fcbde
Alexander Tkachenko <khalt.zommer@gmail.com>
2025-02-08 21:09:51 +0300

    Add EnumerableExtensions class with some extensions

---
+++ Runtime/EnumerableExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SweetAlgy.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> self) 
+            => self is null || !self.Any();
+        
+        public static bool IsNotNullOrEmpty<T>(this IEnumerable<T> self) 
+            => !self.IsNullOrEmpty();
+        
+        public static IEnumerable<T> GetDuplicateItems<T>(this IEnumerable<T> self)
+            => self
+               .GroupBy(item => item)
+               .Where(group => group.Count() > 1)
+               .Select(group => group.Key);
+
+        public static IEnumerable<TResult> GetDuplicateItems<TSource, TKey, TResult>(
+            this IEnumerable<TSource> self,
+            Func<TSource, TKey> keySelector,
+            Func<TKey, TResult> resultSelector)
+            => self
+               .GroupBy(keySelector)
+               .Where(group => group.Count() > 1)
+               .Select(group => resultSelector(group.Key));
+        
+        public static int FindNullItems<T>(this IEnumerable<T> self)
+            => self.Count(item => item is null);
+    }
+}
---
+++ Tests/Runtime/EnumerableExtensionsTest.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+// ReSharper disable ExpressionIsAlwaysNull
+// ReSharper disable CollectionNeverUpdated.Local
+
+namespace SweetAlgy.Extensions.Tests.Runtime
+{
+    [TestFixture]
+    public class EnumerableExtensionsTest
+    {
+        [Test]
+        public void IsNullOrEmptyReturnsTrueForEmptyCollection()
+        {
+            var emptyCollection = new List<int>();
+            Assert.That(emptyCollection.IsNullOrEmpty(), Is.True);
+        }
+
+        [Test]
+        public void IsNullOrEmptyReturnsTrueForNullCollection()
+        {
+            List<int> nullCollection = null;
+            Assert.That(nullCollection.IsNullOrEmpty(), Is.True);
+        }
+
+        [Test]
+        public void IsNotNullOrEmptyReturnsTrueForNonEmptyCollection()
+        {
+            var nonEmptyCollection = new List<int> { 1, 2 };
+            Assert.That(nonEmptyCollection.IsNotNullOrEmpty(), Is.True);
+        }
+
+        [Test]
+        public void GetDuplicateItemsReturnsEmptyWhenNoDuplicatesExist()
+        {
+            var collection = new List<int> { 1, 2, 3, 4, 5 };
+            Assert.That(collection.GetDuplicateItems(), Is.Empty);
+        }
+
+        [Test]
+        public void GetDuplicateItemsReturnsDuplicateItems()
+        {
+            var collection = new List<int> { 1, 2, 3, 4, 3, 5, 1 };
+            Assert.That(collection.GetDuplicateItems(), Is.EquivalentTo(new List<int> { 1, 3 }));
+        }
+
+        [Test]
+        public void GetDuplicatesItemsWithSelectorsTransformsDuplicates()
+        {
+            var items = new List<string> { "apple", "banana", "apple", "cherry", "banana" };
+            Assert.That(items.GetDuplicateItems(item => item, key => key.ToUpper()),
+                Is.EquivalentTo(new List<string> { "APPLE", "BANANA" }));
+        }
+    }
+}
